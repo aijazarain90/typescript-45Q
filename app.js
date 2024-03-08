781fc9a875e9ce8df0bff4468dd7a163f0360c66
aijazarain90 <156091792+aijazarain90@users.noreply.github.com>
2024-03-09 02:14:49 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-// Array storing names of friends
-var names = ["shahrukh", "adil", "ramzan", "asad",];
-// Print each person's name by accessing each element in the list, one at a time
-for (var i = 0; i < names.length; i++) {
-    console.log("Friend " + (i + 1) + ": " + names[i]);
+// Original guest list
+var guestList = ["noman rajput", "shahrukh arain", "adil arain"];
+// Display the original guest list
+console.log("Original guest list:");
+for (var i = 0; i < guestList.length; i++) {
+    console.log(guestList[i]);
+}
+// One guest cannot make it, so replacing them with a new guest
+var unableToAttend = "shahrukh arain";
+var newGuest = "hadi arain";
+// Finding the index of the guest who cannot attend
+var indexOfUnableToAttend = guestList.indexOf(unableToAttend);
+// Replacing the guest with the new guest
+if (indexOfUnableToAttend !== -1) {
+    guestList.splice(indexOfUnableToAttend, 1, newGuest);
+}
+else {
+    console.log(unableToAttend + " is not in the guest list.");
+}
+// Display the updated guest list with the new invitation
+console.log("\nUpdated guest list with new invitation:");
+for (var i = 0; i < guestList.length; i++) {
+    console.log(guestList[i]);
 }
