1318b6db140f40c11fe29253ceee4ba0c03e5765
aijazarain90 <156091792+aijazarain90@users.noreply.github.com>
2024-03-09 02:00:50 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-var fav_number = 16;
-var message = ("Here is your fav number");
-console.log(message, fav_number);
+// Array storing names of friends
+var names = ["shahrukh", "adil", "ramzan", "asad",];
+// Print each person's name by accessing each element in the list, one at a time
+for (var i = 0; i < names.length; i++) {
+    console.log("Friend " + (i + 1) + ": " + names[i]);
+}
